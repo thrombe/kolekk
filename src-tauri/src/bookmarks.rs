0028aa93995f8e5020573ca5304be88d9eda1fa0
thrombe <yash.n.thombare620@gmail.com>
2023-03-29 01:32:11 +0530

    refactored bookmarks save code

--- src-tauri/src/bookmarks.rs
+++ src-tauri/src/bookmarks.rs
@@ -9,8 +9,7 @@ use reqwest::Client;
 use tauri::{http::Uri, Manager, State};
 
 use crate::{
-    bad_error::{BadError, Error, InferBadError, Inspectable},
-    config::AppConfig,
+    bad_error::{Error, InferBadError, Inspectable},
     database::AppDatabase,
 };
 
@@ -29,82 +28,22 @@ pub async fn save_bookmark(
         .or_else(|| data.text_html.as_ref().map(|h| todo!())) // parse all potential urls if the text does not exist
         .unwrap_or_default()
         .into_iter()
-        // .filter_map(|u| Uri::from_str(u).ok().map(|_| u))
         .map(|s| s.trim_matches(&['.', ' '][..]))
         .map(ToOwned::to_owned)
         .map(|u| async {
             let id = 0;
-
-            let b = if u.starts_with("- [") {
-                let title_start = u.find('[').unwrap();
-                if let Some(title_end) = u.find("](") {
-                    let url_start = u[title_end..].find('(');
-                    let url_end = u[title_end..].find(')');
-                    match (url_start, url_end) {
-                        (Some(start), Some(end)) => {
-                            // Some(&u[title_end..][start..end+1])
-                            let b = Bookmark {
-                                url: u[title_end..][start+1..end].into(),
-                                title: Some(u[title_start+1..title_end].into()),
-                                description: None,
-                                id,
-                                tags: vec![],
-                                related: vec![],
-                            };
-                            Some(b)
-                        }
-                        _ => None,
-                    }
-                } else {
-                    None
-                }.bad_err("not url")?
-            } else {
-                let title: OptionFuture<_> = client
-                    .inner()
-                    .get(&u)
-                    .send()
-                    .await
-                    .look(|e| dbg!(e))
-                    .ok()
-                    .map(|page| async {
-                        if page
-                            .headers()
-                            .get("content-type")
-                            .and_then(|e| e.to_str().look(|e| dbg!(e)).ok())
-                            .unwrap_or_default()
-                            .contains("text/html")
-                        {
-                            page.text()
-                                .await
-                                .look(|e| dbg!(e))
-                                .ok()
-                                .as_ref()
-                                .and_then(|t| {
-                                    scraper::Html::parse_document(t)
-                                        .select(&scraper::Selector::parse("title").ok()?)
-                                        .next()
-                                        .map(|e| e.inner_html())
-                                        .look(|e| dbg!(e))
-                                })
-                        } else {
-                            None
-                        }
-                    })
-                    .into();
-                Bookmark {
-                    url: u,
-                    title: title.await.unwrap_or_default(),
-                    description: None,
-                    id,
-                    tags: vec![],
-                    related: vec![],
-                }
+            let b = match try_bookmark_from_markdown_url(id, &u) {
+                Some(u) => Some(u),
+                None => try_bookmark_from_url(id, u, client.inner()).await.ok(),
             };
-            crate::database::add_bookmark(db.inner(), b)
-                .await
-                .infer_err()?;
-            app.emit_all("item-added", id).infer_err()?;
-            Ok(id)
+
+            if let Some(b) = b {
+                crate::database::add_bookmark(db.inner(), b)
+                    .await
+                    .infer_err()?;
+                app.emit_all("item-added", id).infer_err()?;
+            }
+            Ok(())
         })
         .collect::<FuturesUnordered<_>>()
         .collect::<Vec<Result<_, _>>>()
@@ -112,4 +51,73 @@ pub async fn save_bookmark(
         .into_iter()
         .collect::<Result<Vec<_>, _>>()?;
     Ok(())
+}
+
+pub fn try_bookmark_from_markdown_url(id: u32, u: impl AsRef<str>) -> Option<Bookmark> {
+    let u = u.as_ref();
+    if u.starts_with("- [") {
+        let title_start = u.find('[').unwrap();
+        if let Some(title_end) = u.find("](") {
+            let url_start = u[title_end..].find('(');
+            let url_end = u[title_end..].find(')');
+            if let (Some(start), Some(end)) = (url_start, url_end) {
+                let url = &u[title_end..][start + 1..end];
+                let _ = Uri::from_str(url).ok()?;
+                let b = Bookmark {
+                    url: url.into(),
+                    title: Some(u[title_start + 1..title_end].into()),
+                    description: None,
+                    id,
+                    tags: vec![],
+                    related: vec![],
+                };
+                return Some(b);
+            }
+        }
+    }
+    None
+}
+
+pub async fn try_bookmark_from_url(id: u32, u: String, client: &Client) -> Result<Bookmark, Error> {
+    let _ = Uri::from_str(&u).infer_err()?;
+    let title: OptionFuture<_> = client
+        .get(&u)
+        .send()
+        .await
+        .look(|e| dbg!(e))
+        .ok()
+        .map(|page| async {
+            if page
+                .headers()
+                .get("content-type")
+                .and_then(|e| e.to_str().look(|e| dbg!(e)).ok())
+                .unwrap_or_default()
+                .contains("text/html")
+            {
+                page.text()
+                    .await
+                    .look(|e| dbg!(e))
+                    .ok()
+                    .as_ref()
+                    .and_then(|t| {
+                        scraper::Html::parse_document(t)
+                            .select(&scraper::Selector::parse("title").ok()?)
+                            .next()
+                            .map(|e| e.inner_html())
+                            .look(|e| dbg!(e))
+                    })
+            } else {
+                None
+            }
+        })
+        .into();
+    let b = Bookmark {
+        url: u,
+        title: title.await.unwrap_or_default(),
+        description: None,
+        id,
+        tags: vec![],
+        related: vec![],
+    };
+    Ok(b)
 }
