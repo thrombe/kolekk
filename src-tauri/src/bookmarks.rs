7eef0bff11793070be8e4d81d624ab435fa6017f
thrombe <yash.n.thombare620@gmail.com>
2023-03-29 01:31:33 +0530

    try to parse simple markdown urls

--- src-tauri/src/bookmarks.rs
+++ src-tauri/src/bookmarks.rs
@@ -9,7 +9,7 @@ use reqwest::Client;
 use tauri::{http::Uri, Manager, State};
 
 use crate::{
-    bad_error::{Error, InferBadError, Inspectable},
+    bad_error::{BadError, Error, InferBadError, Inspectable},
     config::AppConfig,
     database::AppDatabase,
 };
@@ -29,49 +29,76 @@ pub async fn save_bookmark(
         .or_else(|| data.text_html.as_ref().map(|h| todo!())) // parse all potential urls if the text does not exist
         .unwrap_or_default()
         .into_iter()
-        .filter_map(|u| Uri::from_str(u).ok().map(|_| u))
+        // .filter_map(|u| Uri::from_str(u).ok().map(|_| u))
+        .map(|s| s.trim_matches(&['.', ' '][..]))
         .map(ToOwned::to_owned)
         .map(|u| async {
-            let title: OptionFuture<_> = client
-                .inner()
-                .get(&u)
-                .send()
-                .await
-                .look(|e| dbg!(e))
-                .ok()
-                .map(|page| async {
-                    if page
-                        .headers()
-                        .get("content-type")
-                        .and_then(|e| e.to_str().look(|e| dbg!(e)).ok())
-                        .unwrap_or_default()
-                        .contains("text/html")
-                    {
-                        page.text()
-                            .await
-                            .look(|e| dbg!(e))
-                            .ok()
-                            .as_ref()
-                            .and_then(|t| {
-                                scraper::Html::parse_document(t)
-                                    .select(&scraper::Selector::parse("title").ok()?)
-                                    .next()
-                                    .map(|e| e.inner_html())
-                                    .look(|e| dbg!(e))
-                            })
-                    } else {
-                        None
-                    }
-                })
-                .into();
             let id = 0;
-            let b = Bookmark {
-                url: u,
-                title: title.await.unwrap_or_default(),
-                description: None,
-                id,
-                tags: vec![],
-                related: vec![],
+
+            let b = if u.starts_with("- [") {
+                let title_start = u.find('[').unwrap();
+                if let Some(title_end) = u.find("](") {
+                    let url_start = u[title_end..].find('(');
+                    let url_end = u[title_end..].find(')');
+                    match (url_start, url_end) {
+                        (Some(start), Some(end)) => {
+                            // Some(&u[title_end..][start..end+1])
+                            let b = Bookmark {
+                                url: u[title_end..][start+1..end].into(),
+                                title: Some(u[title_start+1..title_end].into()),
+                                description: None,
+                                id,
+                                tags: vec![],
+                                related: vec![],
+                            };
+                            Some(b)
+                        }
+                        _ => None,
+                    }
+                } else {
+                    None
+                }.bad_err("not url")?
+            } else {
+                let title: OptionFuture<_> = client
+                    .inner()
+                    .get(&u)
+                    .send()
+                    .await
+                    .look(|e| dbg!(e))
+                    .ok()
+                    .map(|page| async {
+                        if page
+                            .headers()
+                            .get("content-type")
+                            .and_then(|e| e.to_str().look(|e| dbg!(e)).ok())
+                            .unwrap_or_default()
+                            .contains("text/html")
+                        {
+                            page.text()
+                                .await
+                                .look(|e| dbg!(e))
+                                .ok()
+                                .as_ref()
+                                .and_then(|t| {
+                                    scraper::Html::parse_document(t)
+                                        .select(&scraper::Selector::parse("title").ok()?)
+                                        .next()
+                                        .map(|e| e.inner_html())
+                                        .look(|e| dbg!(e))
+                                })
+                        } else {
+                            None
+                        }
+                    })
+                    .into();
+                Bookmark {
+                    url: u,
+                    title: title.await.unwrap_or_default(),
+                    description: None,
+                    id,
+                    tags: vec![],
+                    related: vec![],
+                }
             };
             crate::database::add_bookmark(db.inner(), b)
                 .await
