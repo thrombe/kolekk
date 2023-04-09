f0b03135cebce4654d3842b0a1b52c80c8bef990
thrombe <yash.n.thombare620@gmail.com>
2023-04-10 03:25:31 +0530

    set ignore_internal_cost in stretto

--- src-tauri/src/images.rs
+++ src-tauri/src/images.rs
@@ -6,7 +6,6 @@ use std::{
     fmt::Debug,
     fs::File,
     io::{BufWriter, Cursor, Write},
-    num::NonZeroUsize,
     path::{Path, PathBuf},
     str::FromStr,
     sync::Mutex,
@@ -103,14 +102,13 @@ pub async fn image_thumbnail(
     let width = width.round() as u32;
 
     let dir = thumbnailer.dir.clone();
-    let tmb = { thumbnailer.cache.lock().infer_err()?.get(&uri).cloned() };
+    let tmb = { thumbnailer.cache.get(&uri).map(|v| v.value().clone())  };
+    dbg!(&tmb, thumbnailer.cache.len());
     if let Some(tmb) = tmb {
         let img = tokio::task::spawn_blocking(move || {
             let img = tmb.get_image(ThumbnailSize::get_appropriate_size(width), &dir)?;
             Ok(img)
-        })
-        .await
-        .infer_err()??;
+        }).await.infer_err()??;
         return Ok(img.to_string_lossy().to_string());
     }
 
@@ -118,27 +116,23 @@ pub async fn image_thumbnail(
     let u = uri.clone();
     let (tmb, img) = tokio::task::spawn_blocking(move || async move {
         let tmb = Thumbnail::new(&u, &dir, &client)
-            .await?
-            .look(|e| dbg!(e))
-            .bad_err("coule not get an image from the uri")?;
+        .await?
+        .look(|e| dbg!(e))
+        .bad_err("coule not get an image from the uri")?;
         let img = tmb.get_image(ThumbnailSize::get_appropriate_size(width), &dir)?;
         Ok((tmb, img))
-    })
-    .await
-    .infer_err()?
-    .await?;
+    }).await.infer_err()?.await?;
 
-    let old = { thumbnailer.cache.lock().infer_err()?.push(uri, tmb) };
-    if let Some((_uri, tmb)) = old {
-        tmb.delete(&thumbnailer.dir)?;
-    }
+    { thumbnailer.cache.insert(uri.clone(), tmb, 1).await }.look(|e| dbg!(e));
+    thumbnailer.cache.wait().await.infer_err()?;
+    {thumbnailer.cache.get(&uri).map(|v| v.value().clone()).look(|e| dbg!(e, thumbnailer.cache.len()));}
 
     Ok(img.to_string_lossy().to_string())
 }
 
 pub struct Thumbnailer {
     dir: PathBuf,
-    cache: Mutex<LruCache<String, Thumbnail>>,
+    cache: AsyncCache<String, Thumbnail>,
 }
 impl Thumbnailer {
     pub fn new(dir: impl AsRef<Path>) -> Result<Self, Error> {
@@ -148,7 +142,7 @@ impl Thumbnailer {
         }
         Ok(Self {
             dir,
-            cache: Mutex::new(LruCache::new(NonZeroUsize::new(5000).unwrap())),
+            cache: AsyncCache::builder(50000, 5000).set_ignore_internal_cost(true).finalize(tokio::spawn).infer_err()?,
         })
     }
 }
@@ -236,6 +230,7 @@ impl Thumbnail {
 
         if size.value().map(|v| v < self.width).unwrap_or(false) {
             // thumbnail
+            dbg!("creating new thumbnail!!!");
             let thumbnail = path.join(size.as_ref());
             if !thumbnail.exists() {
                 let s = size
