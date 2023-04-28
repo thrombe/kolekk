2e184e1ebfcf3caa8e44e40a4743d8036ebd490b
thrombe <yash.n.thombare620@gmail.com>
2023-04-29 02:58:01 +0530

    use VirtualScrollable in /hoard/reaction_images

--- src/routes/hoard/reaction_images/+page.svelte
+++ src/routes/hoard/reaction_images/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts" context="module">
+    import { Searcher } from '$lib/commands';
     import { writable } from 'svelte/store';
 
-    let searcher = writable(new Searcher<Image>("Image", 50));
+    let searcher = writable(new Searcher<Image>('Image', 50));
+    let selected = writable(0);
+    let search_query = writable('');
 </script>
 
 <script lang="ts">
@@ -9,18 +12,27 @@
     import type { DragDropPaste, Image, Indexed } from 'types';
     import DataListener from '$lib/DataListener.svelte';
     import { files_to_bytearrays } from '$lib/data_listener';
-    import { get_path, Searcher } from '$lib/commands';
+    import VirtualScrollable from '$lib/VirtualScrollable.svelte';
+    import Card from './Card.svelte';
+    import { tick } from 'svelte';
 
     const file_drop = async (e: DragDropPaste<File>) => {
         let images: Image[] = await invoke('get_images', { data: await files_to_bytearrays(e) });
         console.log(images);
-        $searcher.add_item(...images.map(img => {
-            let searchable: Indexed[] = img.title ? [{ data: img.title, field: "Text" }] : [];
-            return { data: img , searchable }
-        }));
+        $searcher.add_item(
+            ...images.map((img) => {
+                let searchable: Indexed[] = img.title ? [{ data: img.title, field: 'Text' }] : [];
+                return { data: img, searchable };
+            })
+        );
     };
 
     $searcher.next_page();
+    $searcher.on_update = async (e: Searcher<Image>) => {
+        items = e.search_results.map((e) => {
+            return { id: e.id, data: e };
+        });
+    };
 
     let tag_name = '';
     const add_tag = async () => {
@@ -38,196 +50,99 @@
         // search_images();
     };
 
-    let width = 100;
-    let query = '';
+    let window_width = 100;
+    let item_aspect_ratio = 1;
     let search_images = () => {
-        $searcher.query = query;
-        console.log(images, $searcher.search_results.map(e => e.data.data.title), $searcher.search_results.map(e => e.id))
+        $searcher.query = $search_query;
+        console.log(
+            images,
+            $searcher.search_results.map((e) => e.data.data.title),
+            $searcher.search_results.map((e) => e.id)
+        );
     };
     let images = new Array();
 
-    $: $searcher.query = query;
-    $: console.log($searcher.search_results.map(e => e.data.data.title));
-    $: images = $searcher.search_results;
-    $: console.log(images);
+    $: $searcher.query = $search_query;
+    // $: console.log($searcher.search_results.map(e => e.data.data.title));
+    // $: images = $searcher.search_results;
+    // $: console.log(images);
+    // $: items = images.map(e => {
+    //     return { id: e.id, data: e };
+    // });
+    let items = new Array();
+    // searcher.subscribe(e => {
+    //     console.log(e);
+    //     items = e.search_results.map(e => {
+    //         return { id: e.id, data: e };
+    //     });
+    // });
+
+    let end_is_visible = true;
+    let search_input: any;
+    const on_keydown = async (event: KeyboardEvent, scroll_selected_into_view: any) => {
+        if (event.key == 'a') {
+            // await add_tag_button();
+            // event.preventDefault();
+        } else if (event.key == '/') {
+            $selected = 0;
+            await tick();
+            await scroll_selected_into_view();
+            $search_query = '';
+            search_input.focus();
+            event.preventDefault();
+        }
+    };
+
+    const end_reached = async () => {};
 </script>
 
 <DataListener on_receive={file_drop} />
 
-<svelte:window bind:innerWidth={width} />
+<svelte:window bind:innerWidth={window_width} />
+
+<cl class="inputs">
+    <input bind:value={$search_query} on:input={search_images} bind:this={search_input} />
+    <button on:click={search_images}>refresh</button>
+
+    <input bind:value={tag_name} />
+    <button on:click={add_tag}>add tag</button>
+    <button on:click={remove_tag}>remove tag</button>
+</cl>
 
 <cl>
-    <buttons>
-        <input bind:value={query} on:input={search_images} />
-        <button on:click={search_images}>refresh</button>
-
-        <input bind:value={tag_name} />
-        <button on:click={add_tag}>add tag</button>
-        <button on:click={remove_tag}>remove tag</button>
-    </buttons>
-    {#each images as img (img.id)}
-        <card-div draggable="true" style="height:{width / 5}px; width: {width / 5}px">
-            <card-insides draggable="true">
-                <image-div>
-                    {#await get_path(img.data.data.path)}
-                        <img draggable="false" src={''} alt="" />
-                    {:then path} 
-                        <img draggable="false" src={path} alt="" />
-                    {/await}
-                </image-div>
-                {#if img.data.data.title.length > 0}
-                    <span class="title">
-                        {img.data.data.title}
-                    </span>
-                {/if}
-                {#if img.data.tags.length > 0}
-                    <tags-div>
-                        <tag-padding>{'a'}</tag-padding>
-                        {#each img.data.tags as tag}
-                            <tag>{tag}</tag>
-                        {/each}
-                    </tags-div>
-                {/if}
-            </card-insides>
-        </card-div>
-    {/each}
+    <VirtualScrollable
+        bind:items
+        columns={5}
+        width={window_width}
+        item_height={window_width / 5 / item_aspect_ratio}
+        {end_reached}
+        bind:selected={$selected}
+        {on_keydown}
+        bind:end_is_visible
+        let:item_width={width}
+        let:root
+        let:item={image}
+        let:selected={s}
+    >
+        <Card {width} aspect_ratio={item_aspect_ratio} selected={s} {image} {root} />
+    </VirtualScrollable>
 </cl>
 
 <style>
-    image-div {
-        width: 100%;
-        /* height: 100%; */
-        max-height: 100%;
-        height: calc(100%);
-        overflow: hidden;
+    * {
+        --input-height: 33px;
     }
 
-    image-div img {
-        /* border-radius: 15px; */
-        /* border-bottom-left-radius: 0px; */
-        /* border-bottom-right-radius: 0px; */
-
-        width: calc(100%);
-        height: calc(100%);
-        object-fit: cover;
-    }
-
-    .title {
-        font-size: 1.17ch;
-        padding-bottom: 0.556ch;
-        font-weight: 500;
-        width: calc(100%);
-        height: min-content;
-
-        text-align: center;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        white-space: nowrap;
-
-        color: aquamarine;
-        /* background-color: blue; */
-    }
-
-    card-div {
-        overflow: hidden;
-    }
-
-    card-insides {
-        /* width: 100%; */
-        /* height: calc(100% - 2px - 6px); */
-        height: calc(100% - 0px - 5px);
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        color: rgb(179, 179, 179);
-        margin-left: 3px;
-        margin-right: 3px;
-        margin-top: 3px;
-        margin-bottom: 3px;
-
-        background-color: blueviolet;
-
-        border: 1px solid;
-        border-radius: 15px;
-        border-color: red;
-
-        overflow: hidden;
-    }
-
-    card-div + card-div {
-    }
-
-    tags-div {
-        display: flex;
-        width: calc(100%);
-        height: min-content;
-        /* overflow-x: auto; */
-        /* overflow-y: hidden; */
-
-        padding: 0;
-        margin: 0;
-        background-color: #ffffff;
-        height: min-content;
-
-        /* flex-wrap: wrap; */
-        /* overflow: hidden; */
-        /* justify-content:space-evenly; */
-    }
-
-    tags-div::-webkit-scrollbar {
-        /* background-color: #630424; */
-        /* display: none; */
-        width: 0;
-        background: transparent;
-        height: 0;
-    }
-
-    tags-div tag {
-        font-size: 1.17ch;
-        font-weight: 700;
-        /* height: 3.0ch; */
-
-        padding-left: 3px;
-        padding-right: 3px;
-        padding-bottom: 0.57ch;
-
-        background-color: #5b931b;
-        color: #630424;
-        border-radius: 3px;
-        width: min-content;
-    }
-
-    tags-div tag + tag {
-        margin-left: 1.5%;
-    }
-
-    buttons {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        width: 100%;
-        height: 33px;
-    }
-
-    tag-padding {
-        /* width: 12px; */
-        height: 1px;
-        /* height: 3.0ch; */
-        color: transparent;
+    .inputs {
+        height: var(--input-height);
     }
 
     cl {
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        overflow: auto;
+        overflow: hidden;
         width: 100%;
-        /* height: 100%; */
-    }
-    rw {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
+        height: calc(100% - var(--input-height));
     }
 </style>
---
+++ src/routes/hoard/reaction_images/Card.svelte
@@ -0,0 +1,154 @@
+<script lang="ts">
+    import { get_path } from '$lib/commands';
+    import ImageCard from '$lib/ImageCard.svelte';
+    import { invoke } from '@tauri-apps/api';
+    import type { Meta, Taggable, Image } from 'types';
+
+    export let width: number;
+    export let aspect_ratio: number;
+    export let image: Meta<Taggable<Image>>;
+    
+    export let selected = false;
+    export let on_click: any = () => {};
+    export let root: HTMLElement | null = null;
+
+    let title = image.data.data.title;
+    let img_source = '';
+    (async () => {
+        img_source = await get_path(image.data.data.path);
+        console.log(img_source);
+    })();
+
+    let ele: HTMLElement;
+    $: if (ele) {
+        let color = selected ? '#558855' : '#885555';
+        ele.style.setProperty('--color', color);
+        ele.style.setProperty('--color-transparent', color + '00');
+        ele.style.setProperty('--border', '2px');
+        ele.style.setProperty('--border-radius', '15px');
+    }
+    let insides: HTMLElement;
+    $: if (insides) {
+        insides.style.width = width.toString() + 'px';
+        let height = width / aspect_ratio;
+        insides.style.height = height.toString() + 'px';
+    }
+
+    $: wrap = selected ? 'normal' : 'nowrap';
+    $: shade_height = selected ? '60%' : '25%';
+    let bg_color = '#282828';
+
+    const copy = async () => {
+        await invoke('copy_image_to_clipboard', { imgPath: image.data.data.path });
+    };
+</script>
+
+<this-helps-position-the-title>
+    <cl bind:this={ele} draggable="true" on:click={on_click} on:keydown={() => {}}>
+        <card-div bind:this={insides}>
+            <card-insides>
+                <ImageCard {img_source} {width} {aspect_ratio} {bg_color} lazy={false} {root} />
+
+                {#if title && title.length > 0}
+                    <title-box style={'height: ' + shade_height}>
+                        <span style={'white-space: ' + wrap}>{title}</span>
+                    </title-box>
+                {/if}
+
+                <button class="copy-button" on:click={copy}>
+                    <span>copy</span>
+                </button>
+            </card-insides>
+        </card-div>
+    </cl>
+</this-helps-position-the-title>
+
+<style>
+    .copy-button {
+        --width: 40px;
+        --height: 20px;
+        position: absolute;
+        z-index: 2;
+        float: left;
+        height: var(--height);
+        width: var(--width);
+        top: calc(var(--height) / 2);
+        left: calc(var(--height) / 2);
+        border: 2px solid;
+        border-radius: 8px;
+        border-color: var(--color);
+        padding: 0px;
+        margin: 0px;
+        transition: width 0.2s ease;
+        text-align: center;
+        line-height: calc(var(--height) / 2);
+        font-size: 1.37ch;
+        font-weight: 700;
+        color: #282828;
+        background-color: var(--color);
+        line-height: 2.3ch;
+    }
+
+    .copy-button:hover {
+        color: #d8d8d8;
+        background-color: #558855af;
+    }
+
+    this-helps-position-the-title {
+        position: relative;
+    }
+
+    title-box {
+        position: absolute;
+        bottom: 0;
+        width: calc(100% - 3 * var(--border));
+        background-image: linear-gradient(to top, var(--color), var(--color-transparent));
+        border-radius: var(--border-radius);
+        margin-bottom: 3px;
+        overflow: hidden;
+        left: var(--border);
+    }
+
+    title-box span {
+        position: absolute;
+        bottom: 0;
+        width: calc(100% - 6 * var(--border));
+        padding-left: calc(3 * var(--border));
+        padding-right: calc(3 * var(--border));
+        border-radius: var(--border-radius);
+        text-align: center;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-size: 1.37ch;
+        padding-bottom: 0.456ch;
+        font-weight: 550;
+        color: #cccccc;
+    }
+
+    card-insides {
+        width: calc(100% - 2 * var(--border) - 6px);
+        height: calc(100% - 2 * var(--border) - 6px);
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: var(--color);
+        margin-left: 3px;
+        margin-right: 3px;
+        margin-top: 3px;
+        margin-bottom: 3px;
+
+        border: var(--border) solid;
+        border-radius: var(--border-radius);
+
+        overflow: hidden;
+    }
+
+    cl {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        overflow: auto;
+        width: 100%;
+    }
+</style>
