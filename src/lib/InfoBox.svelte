f6c6282cda4a2265ac0a491986d04756ccc566b6
thrombe <yash.n.thombare620@gmail.com>
2023-05-07 04:06:18 +0530

    added InfoBox, added tagging stuff to /hoard/reaction_images

---
+++ src/lib/InfoBox.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+    import ImageCard from './ImageCard.svelte';
+
+    export let item: any;
+    export let width: number;
+    export let get_img_source = async (): Promise<string> => {
+        return '';
+    };
+
+    let img_source = '';
+    (async () => {
+        img_source = await get_img_source();
+    })();
+
+    let height = 100;
+    let border_radius = 1;
+</script>
+
+<info-box
+    draggable="true"
+    bind:clientHeight={height}
+    style="--border-radius: {border_radius}px;
+        --width: {width - border_radius * 2}px;
+        --height: {height - border_radius * 2}px;"
+>
+    <rel>
+        <abs>
+            <krop>
+                <ImageCard
+                    width={width - border_radius * 2}
+                    aspect_ratio={(width - border_radius * 2) / (height - border_radius * 2)}
+                    lazy={false}
+                    {img_source}
+                />
+            </krop>
+        </abs>
+        <abs class="bg" />
+    </rel>
+
+    <fg>
+        <slot {item} />
+    </fg>
+</info-box>
+
+<style>
+    info-box {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+    }
+
+    rel {
+        position: relative;
+    }
+
+    abs {
+        width: var(--width);
+        height: var(--height);
+        position: absolute;
+        top: 0px;
+        left: 0px;
+
+        border: var(--border-radius) solid;
+        border-color: #aa99aa;
+    }
+
+    krop {
+        display: block;
+        -webkit-filter: brightness(50%) blur(8px);
+        overflow: hidden;
+        z-index: -2;
+    }
+
+    .bg {
+        z-index: 1;
+        background-color: #22222288;
+    }
+
+    fg {
+        z-index: 2;
+        width: var(--width);
+        height: var(--height);
+        margin: var(--border-radius);
+    }
+</style>
--- src/routes/hoard/reaction_images/+page.svelte
+++ src/routes/hoard/reaction_images/+page.svelte
@@ -1,20 +1,25 @@
 <script lang="ts" context="module">
-    import { get_path, Searcher } from '$lib/commands';
+    import { get_path, new_searcher, type RSearcher, type RObject } from '$lib/commands';
     import { writable } from 'svelte/store';
 
-    let searcher = writable(new Searcher<Image>('Image', 50));
+    let searcher = writable(new_searcher<Image>('Image', 50));
     let selected = writable(0);
     let search_query = writable('');
+
+    let tag_searcher = writable(new_searcher<Tag>('Tag', 50));
+    let tag_query = writable('');
 </script>
 
 <script lang="ts">
     import { invoke } from '@tauri-apps/api/tauri';
-    import type { DragDropPaste, Image, Indexed, Path } from 'types';
+    import type { DragDropPaste, Image, Indexed, Path, Tag } from 'types';
     import DataListener from '$lib/DataListener.svelte';
     import { files_to_bytearrays } from '$lib/data_listener';
     import VirtualScrollable from '$lib/VirtualScrollable.svelte';
     import Card from '$lib/Card.svelte';
     import { tick } from 'svelte';
+    import InfoBox from '$lib/InfoBox.svelte';
+    import type { Unique } from '$lib/virtual';
 
     const file_drop = async (e: DragDropPaste<File>) => {
         let images: Image[] = await invoke('get_images', { data: await files_to_bytearrays(e) });
@@ -28,26 +33,10 @@
     };
 
     $searcher.next_page();
-    $searcher.on_update = async (e: Searcher<Image>) => {
+    $searcher.on_update = async (e: RSearcher<Image>) => {
         items = e.search_results.map((e) => {
             return { id: e.id, data: e };
         });
-    };
-
-    let tag_name = '';
-    const add_tag = async () => {
-        if (tag_name == '') {
-            return;
-        }
-        // await invoke('add_tag_to_image', { img: images[0], tag: tag_name });
-        // search_images();
-    };
-    const remove_tag = async () => {
-        if (tag_name == '') {
-            return;
-        }
-        // await invoke('remove_tag_from_image', { img: images[0], tag: tag_name });
-        // search_images();
     };
 
     let window_width = 100;
@@ -61,11 +50,20 @@
 
     let end_is_visible = true;
     let search_input: any;
+    let tag_search_input: any;
     const on_keydown = async (event: KeyboardEvent, scroll_selected_into_view: any) => {
         if (event.key == 'Enter') {
             await copy_selected();
             // await add_tag_button();
             // event.preventDefault();
+        } else if (event.key == 'a') {
+            tag_box_show = true;
+            await tick();
+            tag_search_input.focus();
+            $tag_query = '';
+            event.preventDefault();
+        } else if (event.key == 'Escape') {
+            tag_box_show = false;
         } else if (event.key == '?') {
             $selected = 0;
             await tick();
@@ -103,13 +101,61 @@
         console.log('copied image', img.title);
     };
     const copy_selected = async () => {
-        await copy(items[$selected].data.data.data);
+        await copy(selected_item.data.data.data);
     };
     const on_enter = async (event: KeyboardEvent) => {
         if (event.key == 'Enter') {
             search_input.blur();
         }
     };
+
+    let info_width = 350;
+    let info_margin = 20;
+    let selected_item: Unique<RObject<Image>, string>;
+
+    let searched_tags = new Array();
+    $tag_searcher.on_update = async (e: RSearcher<Tag>) => {
+        searched_tags = e.search_results;
+    };
+    $tag_searcher.set_query($tag_query);
+    const tag_box_input_handle = async (ev: KeyboardEvent, ob: RObject<Image>) => {
+        if (ev.key == 'Enter') {
+            if (
+                ev.ctrlKey &&
+                !$tag_searcher.search_results.map((t) => t.data.name).includes($tag_query)
+            ) {
+                let tag: Tag = {
+                    object_type: 'main_tag',
+                    name: $tag_query
+                };
+                let tag_id = await $tag_searcher.add_tag(tag);
+                await $tag_searcher.add_tag_to_object(ob.id, tag_id);
+                ob.data.tags.push(tag_id);
+                $tag_query = '';
+
+                await $searcher.reload_reader();
+                searched_tags = await $tag_searcher.set_query($tag_query);
+            } else if (searched_tags.length > 0) {
+                let tag_id = searched_tags[0].id;
+                if (!ob.data.tags.includes(tag_id)) {
+                    await $tag_searcher.add_tag_to_object(ob.id, tag_id);
+                    ob.data.tags.push(tag_id);
+                } else {
+                    await $tag_searcher.remove_tag_from_object(ob.id, searched_tags[0].id);
+                    ob.data.tags = ob.data.tags.filter((e) => e != tag_id);
+                }
+
+                await $tag_searcher.reload_reader();
+                $tag_query = '';
+                await $tag_searcher.set_query($tag_query);
+            }
+        }
+
+        // to make svelte detect updates to tags
+        selected_item.data.data.tags = selected_item.data.data.tags;
+    };
+
+    let tag_box_show = false;
 </script>
 
 <DataListener on_receive={file_drop} />
@@ -124,45 +170,240 @@
         on:keydown={on_enter}
     />
     <button on:click={search_images}>refresh</button>
-
-    <input bind:value={tag_name} />
-    <button on:click={add_tag}>add tag</button>
-    <button on:click={remove_tag}>remove tag</button>
     <button>{end_is_visible}</button>
 </cl>
 
-<cl class="scrollable">
-    <VirtualScrollable
-        bind:items
-        gap={15}
-        item_width={150}
-        item_height={170}
-        on_item_click={copy_selected}
-        {end_reached}
-        bind:selected={$selected}
-        {on_keydown}
-        bind:end_is_visible
-        let:item_width={width}
-        let:item_height
-        let:root
-        let:item={image}
-        let:selected={s}
-    >
-        <Card
-            get_img_source={async () => {
-                return await get_path(image.data.data.path);
-            }}
-            title={image.data.data.title}
-            {width}
-            aspect_ratio={width / item_height}
-            selected={s}
-            item={image}
-            {root}
-        />
-    </VirtualScrollable>
+<cl class="main" style="--info-width: {info_width}px; --info-margin: {info_margin}px;">
+    <scrollable>
+        <VirtualScrollable
+            bind:items
+            gap={15}
+            item_width={150}
+            item_height={170}
+            on_item_click={copy_selected}
+            {end_reached}
+            bind:selected={$selected}
+            {on_keydown}
+            bind:end_is_visible
+            bind:selected_item
+            let:item_width={width}
+            let:item_height
+            let:root
+            let:item={image}
+            let:selected={s}
+        >
+            <Card
+                get_img_source={async () => {
+                    return await get_path(image.data.data.path);
+                }}
+                title={''}
+                {width}
+                aspect_ratio={width / item_height}
+                selected={s}
+                item={image}
+                {root}
+            />
+        </VirtualScrollable>
+    </scrollable>
+
+    {#if selected_item}
+        {#key selected_item.id}
+            <info-box>
+                <InfoBox
+                    item={selected_item}
+                    width={info_width - info_margin}
+                    get_img_source={async () => {
+                        return await get_path(selected_item.data.data.data.path);
+                    }}
+                >
+                    <info>
+                        <info-title>
+                            {@html selected_item.data.data.data.title?.replace(
+                                new RegExp('_', 'g'),
+                                '<wbr>_<wbr>'
+                            )}
+                        </info-title>
+
+                        <tags>
+                            {#key selected_item.data.data.tags.length}
+                                {#await $tag_searcher.get_tags_from_ids(...selected_item.data.data.tags) then tags}
+                                    {#each tags as tag}
+                                        <tag
+                                            on:click={async () => {
+                                                if (!tag_box_show) {
+                                                    return;
+                                                }
+                                                await $tag_searcher.remove_tag_from_object(
+                                                    selected_item.data.id,
+                                                    tag.id
+                                                );
+                                                selected_item.data.data.tags =
+                                                    selected_item.data.data.tags.filter(
+                                                        (e) => e != tag.id
+                                                    );
+                                            }}
+                                            on:keydown={() => {}}
+                                        >
+                                            {tag.data.name}
+                                        </tag>
+                                    {/each}
+                                {/await}
+                            {/key}
+                        </tags>
+                    </info>
+                </InfoBox>
+            </info-box>
+        {/key}
+    {/if}
+
+    {#if tag_box_show}
+        <tag-box>
+            <input
+                bind:this={tag_search_input}
+                bind:value={$tag_query}
+                placeholder="Search"
+                on:input={async () => await $tag_searcher.set_query($tag_query)}
+                on:keydown={async (e) => {
+                    await tag_box_input_handle(e, selected_item.data);
+                }}
+            />
+            <tags>
+                {#each searched_tags as tag (tag.id)}
+                    {#if selected_item.data.data.tags.includes(tag.id)}
+                        <tag
+                            class="contains"
+                            on:click={async () => {
+                                await $tag_searcher.remove_tag_from_object(
+                                    selected_item.data.id,
+                                    searched_tags[0].id
+                                );
+                                selected_item.data.data.tags = selected_item.data.data.tags.filter(
+                                    (e) => e != tag.id
+                                );
+
+                                // to make svelte detect updates to tags
+                                selected_item.data.data.tags = selected_item.data.data.tags;
+                            }}
+                            on:keydown={() => {}}
+                        >
+                            {tag.data.name}
+                        </tag>
+                    {:else}
+                        <tag
+                            on:click={async () => {
+                                await $tag_searcher.add_tag_to_object(
+                                    selected_item.data.id,
+                                    tag.id
+                                );
+                                selected_item.data.data.tags.push(tag.id);
+
+                                // to make svelte detect updates to tags
+                                selected_item.data.data.tags = selected_item.data.data.tags;
+                            }}
+                            on:keydown={() => {}}
+                        >
+                            {tag.data.name}
+                        </tag>
+                    {/if}
+                {/each}
+            </tags>
+        </tag-box>
+    {/if}
 </cl>
 
 <style>
+    tag-box {
+        display: flex;
+        flex-direction: column;
+        --padding: 15px;
+
+        position: absolute;
+        top: calc(var(--input-height) + var(--top-margin));
+        left: var(--gap);
+        width: calc(100% - var(--info-width) - var(--gap) * 2 - var(--padding) * 2);
+        height: calc(100% - var(--gap) - var(--input-height) - 5px - var(--padding) * 2);
+        padding: var(--padding);
+        row-gap: var(--padding);
+
+        border: 1px solid;
+        border-color: #cccccc;
+        background-color: #443944cc;
+        -webkit-backdrop-filter: blur(3px);
+    }
+
+    tag-box input {
+        margin-left: 7%;
+        margin-right: 7%;
+        height: var(--input-height);
+        border: 1px solid;
+        border-color: #666666;
+        background-color: #00000055;
+        border-radius: 9px;
+        padding-left: 20px;
+        padding-right: 20px;
+        font-size: 1rem;
+        color: #aaaaaa;
+        -webkit-backdrop-filter: blur(10px);
+    }
+    tag-box input:focus {
+        outline: none;
+        border-color: #999999;
+    }
+    tag-box input::-webkit-input-placeholder {
+        color: #777777;
+    }
+
+    tags {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+
+        column-gap: 7px;
+        row-gap: 6px;
+
+        overflow-y: auto;
+        overscroll-behavior-block: contain;
+    }
+
+    tag {
+        display: block;
+        border: 1px solid;
+        border-color: #666666;
+        background-color: #00000055;
+        border-radius: 9px;
+        font-size: 1rem;
+        color: #aaaaaa;
+        -webkit-backdrop-filter: blur(10px);
+
+        padding-left: 12px;
+        padding-right: 12px;
+        padding-top: 3.5px;
+        padding-bottom: 3.5px;
+    }
+    tags .contains {
+        border-color: #668866;
+        background-color: #00220055;
+    }
+
+    info {
+        display: flex;
+        flex-direction: column;
+        row-gap: 15px;
+
+        --margin: 15px;
+        margin: var(--margin);
+        width: calc(100% - var(--margin) * 2);
+        height: calc(100% - var(--margin) * 2);
+    }
+
+    info-title {
+        display: block;
+        font-size: 1.7rem;
+        font-weight: 130;
+        word-wrap: break-all;
+        color: #cccccc;
+    }
+
     * {
         --input-height: 33px;
         --gap: 20px;
@@ -173,19 +414,34 @@
         height: var(--input-height);
     }
 
-    .scrollable {
+    .main {
         margin-left: var(--gap);
         margin-right: var(--gap);
         margin-top: var(--top-margin);
         width: calc(100% - var(--gap) * 2);
         height: calc(100% - var(--input-height) - var(--top-margin));
+
+        flex-direction: column;
     }
 
     cl {
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        overflow: hidden;
         width: 100%;
+    }
+
+    scrollable {
+        width: calc(100% - var(--info-width));
+        height: 100%;
+    }
+
+    info-box {
+        width: calc(var(--info-width) - var(--info-margin));
+        margin-left: auto;
+
+        height: calc(100% - 20px);
+        margin-top: auto;
+        margin-bottom: auto;
     }
 </style>
