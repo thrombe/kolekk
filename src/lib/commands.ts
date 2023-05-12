8f991a52ac17f9c31a66e563ec03c107a7e874eb
thrombe <yash.n.thombare620@gmail.com>
2023-05-12 20:35:57 +0530

    implemented some tachi stuff in commands.ts,  errors++

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -53,7 +53,7 @@ class Searcher<T> {
         this._results_valid = false;
     }
 
-    async reload_reader() {
+    async refresh() {
         await invoke("reload_reader");
     }
 
@@ -69,7 +69,7 @@ class Searcher<T> {
     }
 
     async next_page() {
-        await this.reload_reader();
+        await this.refresh();
         if (this._results_valid) {
             if (!this._has_next_page) {
                 return this.search_results;
@@ -154,56 +154,100 @@ export async function get_path(path: Path) {
 }
 
 
-const extensions_facet = { Temp: '/temp/tachi/extension' };
+const extension_facet = { Temp: '/temp/tachi/extension' };
+const source_facet = { Temp: '/temp/tachi/source' };
 
 class TachiClient {
-    is_init: boolean;
-    ext_searcher: RSearcher<Extension>;
+    extension_searcher: RSearcher<Extension>;
+    source_searcher: RSearcher<MangaSource>;
 
     set on_exts_update(callback: (e: RSearcher<Extension>) => Promise<void>) {
-        this.ext_searcher.on_update = callback;
+        this.extension_searcher.on_update = callback;
     }
 
     constructor() {
-        this.is_init = false;
-        this.ext_searcher = new_searcher(extensions_facet, 50);
+        this.extension_searcher = new_searcher(extension_facet, 50);
+        this.source_searcher = new_searcher(source_facet, 50);
     }
 
     async init() {
         await invoke('init_tachidesk_client');
-        this.is_init = true;
     }
 
-    async get_all_extensions() {
+    static async get_all_extensions() {
         let exts: Extension[] = await invoke('tachidesk_get_all_extensions');
         return exts;
     }
 
-    async get_extension_searcher() {
-        let exts = await this.get_all_extensions();
-        await invoke('delete_facet_objects', { facet: extensions_facet });
-        await this.ext_searcher.add_item(
-            ...exts.map((e) => {
-                let searchable: Indexed[] = [{ data: e.name, field: 'Text' }];
-                return { data: e, searchable };
-            })
-        );
-        return this.ext_searcher;
+    static async get_sources() {
+        let sources: MangaSource[] = await invoke('tachidesk_get_source_list');
+        return sources;
     }
+
+    // async get_extension_searcher() {
+    //     return this.extension_searcher;
+    // }
+
+    // async get_source_searcher() {
+    //     return this.source_searcher;
+    // }
 
     async tachidesk_action(pkgName: string, action: ExtensionAction) {
         // this await waits till the action is complete in the backend
         // https://github.com/Suwayomi/Tachidesk-WebUI/blob/d51150b7848cf7a6596bbba7c015328a578dfd16/src/components/ExtensionCard.tsx#L91
         await invoke('tachidesk_extension_action', { pkgName, action });
     };
+}
 
-    async get_sources() {
-        let sources: MangaSource[] = await invoke('tachidesk_get_source_list');
-        return sources;
+class TachiExtensionSearcher extends Searcher<Extension> {
+
+    async refresh(): Promise<void> {
+        let exts = await TachiClient.get_all_extensions();
+        await invoke('delete_facet_objects', { facet: extension_facet });
+        await super.add_item(
+            ...exts.map((e) => {
+                let searchable: Indexed[] = [{ data: e.name, field: 'Text' }];
+                return { data: e, searchable };
+            })
+        );
+        super.refresh();
     }
 
-    async get_source_filters(sourceId: string) {
-        let filters: SourceFilter[] = await invoke('tachidesk_get_source_filters', { sourceId })
+    // TODO: :(
+    async add_item(...items: SearchableEntry<Extension>[]): Promise<RObjectNotTag<Extension>[]> {
+        throw Error(":(");
+    }
+}
+
+class TachiSourceSearcher extends Searcher<MangaSource> {
+    
+    async refresh(): Promise<void> {
+        let exts = await TachiClient.get_sources();
+        await invoke('delete_facet_objects', { facet: source_facet });
+        await super.add_item(
+            ...exts.map((e) => {
+                let searchable: Indexed[] = [{ data: e.name, field: 'Text' }];
+                return { data: e, searchable };
+            })
+        );
+        super.refresh();
+    }
+
+    // TODO: :(
+    async add_item(...items: SearchableEntry<MangaSource>[]): Promise<RObjectNotTag<MangaSource>[]> {
+        throw Error(":(");
+    }
+}
+
+class TachiSourceExplorer {
+    source: MangaSource;
+
+    constructor(source: MangaSource) {
+        this.source = source;
+    }
+
+    async get_filters() {
+        let filters: SourceFilter[] = await invoke('tachidesk_get_source_filters', { sourceId: this.source.id });
         // https://github.com/Suwayomi/Tachidesk-Server/blob/cde5dc5bfa4ce6cce6d565b41589672a754460c0/server/src/main/kotlin/suwayomi/tachidesk/manga/impl/Search.kt#L137
         // let r = await fetch(`http://localhost:4567/api/v1/source/${$page.params.src_id}/filters`, {
         //     method: 'POST',
@@ -222,45 +266,61 @@ class TachiClient {
         return filters;
     }
 
-    async get_popular_manga(sourceId: string, page: number) {
+    async get_popular_manga(page: number) {
         let search_results: MangaListPage = await invoke('tachidesk_get_popular_manga_list', {
-            sourceId,
+            sourceId: this.source.id,
             page
         });
         return search_results;
     }
 
-    async search_manga(sourceId: string, page: number, query: string) {
+    async search_manga(page: number, query: string) {
         let search_results: MangaListPage = await invoke('tachidesk_search_manga_in', {
-            sourceId,
+            sourceId: this.source.id,
             query,
             page
         });
         return search_results;
     }
+}
 
-    async get_manga(mangaId: number) {
+class TachiMangaExplorer {
+    manga: Manga;
+
+    constructor(manga: Manga) {
+        this.manga = manga;
+    }
+
+    static async get(mangaId: number) {
         let manga: Manga = await invoke('tachidesk_get_manga', { mangaId });
         return manga;
     }
 
-    async get_chapters(mangaId: number) {
-        let chapters: Chapter[] = await invoke('tachidesk_get_manga_chapter_list', { mangaId });
+    async get_chapters() {
+        let chapters: Chapter[] = await invoke('tachidesk_get_manga_chapter_list', { mangaId: this.manga.id });
         return chapters;
     }
 
-    async get_chapter(mangaId: number, chapterIndex: number) {
+    async get_chapter(chapterIndex: number) {
         let chapter: Chapter = await invoke('tachidesk_get_chapter', {
-            mangaId,
+            mangaId: this.manga.id,
             chapterIndex
         });
         return chapter;
     }
+}
 
-    async get_manga_page_url(chapter: Chapter, page: number) {
+class TachiChapterExplorer {
+    chapter: Chapter;
+
+    constructor(chapter: Chapter) {
+        this.chapter = chapter;
+    }
+
+    async get_page_url(page: number) {
         let uri: string = await invoke('tachidesk_get_manga_page_url', {
-            mangaId: chapter.mangaId,
-            chapterIndex: chapter.index,
+            mangaId: this.chapter.mangaId,
+            chapterIndex: this.chapter.index,
             page
         });
         return uri;
